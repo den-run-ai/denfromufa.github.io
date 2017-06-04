b5a1c8e0dc2e83d811217075f1b7b782de53f901
Denis Akhiyarov <denis.akhiyarov@gmail.com>
2017-06-04 15:34:22 -0500

    Nikola auto commit.
    Nikola version: 7.8.6

---
+++ cache/.mako.tmp/index.tmpl.py
@@ -0,0 +1,223 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1496608460.061159
+_enable_loop = True
+_template_filename = 'c:/python/anaconda3_64b/lib/site-packages/nikola/data/themes/base/templates/index.tmpl'
+_template_uri = 'index.tmpl'
+_source_encoding = 'utf-8'
+_exports = ['extra_head', 'content', 'content_header']
+
+
+def _mako_get_namespace(context, name):
+    try:
+        return context.namespaces[(__name__, name)]
+    except KeyError:
+        _mako_generate_namespaces(context)
+        return context.namespaces[(__name__, name)]
+def _mako_generate_namespaces(context):
+    ns = runtime.TemplateNamespace('math', context._clean_inheritance_tokens(), templateuri='math_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'math')] = ns
+
+    ns = runtime.TemplateNamespace('comments', context._clean_inheritance_tokens(), templateuri='comments_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'comments')] = ns
+
+    ns = runtime.TemplateNamespace('pagination', context._clean_inheritance_tokens(), templateuri='pagination_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'pagination')] = ns
+
+    ns = runtime.TemplateNamespace('helper', context._clean_inheritance_tokens(), templateuri='index_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'helper')] = ns
+
+def _mako_inherit(template, context):
+    _mako_generate_namespaces(context)
+    return runtime._inherit_from(context, 'base.tmpl', _template_uri)
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        math = _mako_get_namespace(context, 'math')
+        index_teasers = context.get('index_teasers', UNDEFINED)
+        page_links = context.get('page_links', UNDEFINED)
+        def content_header():
+            return render_content_header(context._locals(__M_locals))
+        def extra_head():
+            return render_extra_head(context._locals(__M_locals))
+        prev_next_links_reversed = context.get('prev_next_links_reversed', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        front_index_header = context.get('front_index_header', UNDEFINED)
+        author_pages_generated = context.get('author_pages_generated', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        pagination = _mako_get_namespace(context, 'pagination')
+        parent = context.get('parent', UNDEFINED)
+        index_file = context.get('index_file', UNDEFINED)
+        nextlink = context.get('nextlink', UNDEFINED)
+        current_page = context.get('current_page', UNDEFINED)
+        def content():
+            return render_content(context._locals(__M_locals))
+        date_format = context.get('date_format', UNDEFINED)
+        permalink = context.get('permalink', UNDEFINED)
+        prevlink = context.get('prevlink', UNDEFINED)
+        pagekind = context.get('pagekind', UNDEFINED)
+        posts = context.get('posts', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        __M_writer = context.writer()
+        __M_writer('\n')
+        __M_writer('\n')
+        __M_writer('\n')
+        __M_writer('\n')
+        __M_writer('\n\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'extra_head'):
+            context['self'].extra_head(**pageargs)
+        
+
+        __M_writer('\n\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_extra_head(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        math = _mako_get_namespace(context, 'math')
+        parent = context.get('parent', UNDEFINED)
+        index_file = context.get('index_file', UNDEFINED)
+        posts = context.get('posts', UNDEFINED)
+        def extra_head():
+            return render_extra_head(context)
+        permalink = context.get('permalink', UNDEFINED)
+        __M_writer = context.writer()
+        __M_writer('\n    ')
+        __M_writer(str(parent.extra_head()))
+        __M_writer('\n')
+        if posts and (permalink == '/' or permalink == '/' + index_file):
+            __M_writer('        <link rel="prefetch" href="')
+            __M_writer(str(posts[0].permalink()))
+            __M_writer('" type="text/html">\n')
+        __M_writer('    ')
+        __M_writer(str(math.math_styles_ifposts(posts)))
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        index_teasers = context.get('index_teasers', UNDEFINED)
+        math = _mako_get_namespace(context, 'math')
+        page_links = context.get('page_links', UNDEFINED)
+        def content_header():
+            return render_content_header(context)
+        prev_next_links_reversed = context.get('prev_next_links_reversed', UNDEFINED)
+        _link = context.get('_link', UNDEFINED)
+        front_index_header = context.get('front_index_header', UNDEFINED)
+        author_pages_generated = context.get('author_pages_generated', UNDEFINED)
+        site_has_comments = context.get('site_has_comments', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        pagination = _mako_get_namespace(context, 'pagination')
+        nextlink = context.get('nextlink', UNDEFINED)
+        current_page = context.get('current_page', UNDEFINED)
+        def content():
+            return render_content(context)
+        date_format = context.get('date_format', UNDEFINED)
+        prevlink = context.get('prevlink', UNDEFINED)
+        pagekind = context.get('pagekind', UNDEFINED)
+        posts = context.get('posts', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        __M_writer = context.writer()
+        __M_writer('\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content_header'):
+            context['self'].content_header(**pageargs)
+        
+
+        __M_writer('\n')
+        if 'main_index' in pagekind:
+            __M_writer('    ')
+            __M_writer(str(front_index_header))
+            __M_writer('\n')
+        if page_links:
+            __M_writer('    ')
+            __M_writer(str(pagination.page_navigation(current_page, page_links, prevlink, nextlink, prev_next_links_reversed)))
+            __M_writer('\n')
+        __M_writer('<div class="postindex">\n')
+        for post in posts:
+            __M_writer('    <article class="h-entry post-')
+            __M_writer(str(post.meta('type')))
+            __M_writer('">\n    <header>\n        <h1 class="p-name entry-title"><a href="')
+            __M_writer(str(post.permalink()))
+            __M_writer('" class="u-url">')
+            __M_writer(filters.html_escape(str(post.title())))
+            __M_writer('</a></h1>\n        <div class="metadata">\n            <p class="byline author vcard"><span class="byline-name fn" itemprop="author">\n')
+            if author_pages_generated:
+                __M_writer('                <a href="')
+                __M_writer(str(_link('author', post.author())))
+                __M_writer('">')
+                __M_writer(filters.html_escape(str(post.author())))
+                __M_writer('</a>\n')
+            else:
+                __M_writer('                ')
+                __M_writer(filters.html_escape(str(post.author())))
+                __M_writer('\n')
+            __M_writer('            </span></p>\n            <p class="dateline"><a href="')
+            __M_writer(str(post.permalink()))
+            __M_writer('" rel="bookmark"><time class="published dt-published" datetime="')
+            __M_writer(str(post.formatted_date('webiso')))
+            __M_writer('" title="')
+            __M_writer(filters.html_escape(str(post.formatted_date(date_format))))
+            __M_writer('">')
+            __M_writer(filters.html_escape(str(post.formatted_date(date_format))))
+            __M_writer('</time></a></p>\n')
+            if not post.meta('nocomments') and site_has_comments:
+                __M_writer('                <p class="commentline">')
+                __M_writer(str(comments.comment_link(post.permalink(), post._base_path)))
+                __M_writer('\n')
+            __M_writer('        </div>\n    </header>\n')
+            if index_teasers:
+                __M_writer('    <div class="p-summary entry-summary">\n    ')
+                __M_writer(str(post.text(teaser_only=True)))
+                __M_writer('\n')
+            else:
+                __M_writer('    <div class="e-content entry-content">\n    ')
+                __M_writer(str(post.text(teaser_only=False)))
+                __M_writer('\n')
+            __M_writer('    </div>\n    </article>\n')
+        __M_writer('</div>\n')
+        __M_writer(str(helper.html_pager()))
+        __M_writer('\n')
+        __M_writer(str(comments.comment_link_script()))
+        __M_writer('\n')
+        __M_writer(str(math.math_scripts_ifposts(posts)))
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content_header(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def content_header():
+            return render_content_header(context)
+        __M_writer = context.writer()
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "c:/python/anaconda3_64b/lib/site-packages/nikola/data/themes/base/templates/index.tmpl", "source_encoding": "utf-8", "uri": "index.tmpl", "line_map": {"201": 56, "193": 48, "23": 3, "140": 16, "175": 37, "145": 17, "146": 18, "147": 19, "148": 19, "149": 19, "150": 21, "151": 22, "152": 22, "153": 22, "26": 4, "155": 25, "156": 26, "29": 5, "158": 26, "159": 28, "160": 28, "161": 28, "162": 28, "163": 31, "164": 32, "165": 32, "38": 0, "167": 32, "168": 32, "169": 33, "170": 34, "171": 34, "172": 34, "173": 36, "174": 37, "157": 26, "176": 37, "177": 37, "178": 37, "179": 37, "180": 37, "181": 37, "182": 38, "183": 39, "184": 39, "185": 39, "186": 41, "187": 43, "188": 44, "189": 45, "190": 45, "191": 46, "192": 47, "32": 2, "194": 48, "195": 50, "196": 53, "69": 2, "70": 3, "71": 4, "72": 5, "73": 6, "202": 56, "78": 14, "208": 17, "83": 57, "197": 54, "89": 8, "219": 208, "199": 55, "198": 54, "100": 8, "101": 9, "102": 9, "103": 10, "104": 11, "105": 11, "106": 11, "107": 13, "108": 13, "109": 13, "154": 24, "115": 16, "200": 55, "166": 32}}
+__M_END_METADATA
+"""
---
+++ cache/.mako.tmp/math_helper.tmpl.py
@@ -0,0 +1,144 @@
+# -*- coding:ascii -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1496608459.6890788
+_enable_loop = True
+_template_filename = 'c:/python/anaconda3_64b/lib/site-packages/nikola/data/themes/base/templates/math_helper.tmpl'
+_template_uri = 'math_helper.tmpl'
+_source_encoding = 'ascii'
+_exports = ['math_scripts_ifpost', 'math_styles_ifpost', 'math_scripts', 'math_styles_ifposts', 'math_styles', 'math_scripts_ifposts']
+
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        __M_writer = context.writer()
+        __M_writer('\n\n')
+        __M_writer('\n\n')
+        __M_writer('\n\n')
+        __M_writer('\n\n')
+        __M_writer('\n\n')
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_math_scripts_ifpost(context,post):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def math_scripts():
+            return render_math_scripts(context)
+        __M_writer = context.writer()
+        __M_writer('\n')
+        if post.is_mathjax:
+            __M_writer('    ')
+            __M_writer(str(math_scripts()))
+            __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_math_styles_ifpost(context,post):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def math_styles():
+            return render_math_styles(context)
+        __M_writer = context.writer()
+        __M_writer('\n')
+        if post.is_mathjax:
+            __M_writer('    ')
+            __M_writer(str(math_styles()))
+            __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_math_scripts(context):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        katex_auto_render = context.get('katex_auto_render', UNDEFINED)
+        use_katex = context.get('use_katex', UNDEFINED)
+        mathjax_config = context.get('mathjax_config', UNDEFINED)
+        __M_writer = context.writer()
+        __M_writer('\n')
+        if use_katex:
+            __M_writer('        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.js" integrity="sha384-/y1Nn9+QQAipbNQWU65krzJralCnuOasHncUFXGkdwntGeSvQicrYkiUBwsgUqc1" crossorigin="anonymous"></script>\n        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/contrib/auto-render.min.js" integrity="sha256-ExtbCSBuYA7kq1Pz362ibde9nnsHYPt6JxuxYeZbU+c=" crossorigin="anonymous"></script>\n')
+            if katex_auto_render:
+                __M_writer('            <script>\n                renderMathInElement(document.body,\n                    {\n                        ')
+                __M_writer(str(katex_auto_render))
+                __M_writer('\n                    }\n                );\n            </script>\n')
+            else:
+                __M_writer('            <script>\n                renderMathInElement(document.body);\n            </script>\n')
+        else:
+            __M_writer('        <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML" integrity="sha256-yYfngbEKv4RENfGDvNUqJTqGFcKf31NJEe9OTnnMH3Y=" crossorigin="anonymous"></script>\n')
+            if mathjax_config:
+                __M_writer('        ')
+                __M_writer(str(mathjax_config))
+                __M_writer('\n')
+            else:
+                __M_writer('        <script type="text/x-mathjax-config">\n        MathJax.Hub.Config({tex2jax: {inlineMath: [[\'$latex \',\'$\'], [\'\\\\(\',\'\\\\)\']]}});\n        </script>\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_math_styles_ifposts(context,posts):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        any = context.get('any', UNDEFINED)
+        def math_styles():
+            return render_math_styles(context)
+        __M_writer = context.writer()
+        __M_writer('\n')
+        if any(post.is_mathjax for post in posts):
+            __M_writer('    ')
+            __M_writer(str(math_styles()))
+            __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_math_styles(context):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        use_katex = context.get('use_katex', UNDEFINED)
+        __M_writer = context.writer()
+        __M_writer('\n')
+        if use_katex:
+            __M_writer('        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css" integrity="sha384-wITovz90syo1dJWVh32uuETPVEtGigN07tkttEqPv+uR2SE/mbQcG7ATL28aI9H0" crossorigin="anonymous">\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_math_scripts_ifposts(context,posts):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        any = context.get('any', UNDEFINED)
+        def math_scripts():
+            return render_math_scripts(context)
+        __M_writer = context.writer()
+        __M_writer('\n')
+        if any(post.is_mathjax for post in posts):
+            __M_writer('    ')
+            __M_writer(str(math_scripts()))
+            __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "c:/python/anaconda3_64b/lib/site-packages/nikola/data/themes/base/templates/math_helper.tmpl", "source_encoding": "ascii", "uri": "math_helper.tmpl", "line_map": {"130": 42, "131": 43, "132": 44, "133": 44, "134": 44, "140": 134, "16": 0, "21": 28, "22": 34, "23": 40, "24": 46, "25": 52, "26": 58, "32": 36, "38": 36, "39": 37, "40": 38, "41": 38, "42": 38, "48": 48, "54": 48, "55": 49, "56": 50, "57": 50, "58": 50, "64": 1, "71": 1, "72": 2, "73": 3, "74": 5, "75": 6, "76": 9, "77": 9, "78": 13, "79": 14, "80": 18, "81": 19, "82": 20, "83": 21, "84": 21, "85": 21, "86": 22, "87": 23, "93": 54, "100": 54, "101": 55, "102": 56, "103": 56, "104": 56, "110": 30, "115": 30, "116": 31, "117": 32, "123": 42}}
+__M_END_METADATA
+"""
